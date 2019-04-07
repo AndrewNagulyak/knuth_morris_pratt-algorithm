4637324d6005e87beacfd980fb8b39aef61954bf
Andrew Naholik <andriinahyliak58@gmail.com>
2019-04-07 19:55:11 +0300

    substing

--- knuth_morris_pratt/Program.cs
+++ knuth_morris_pratt/Program.cs
@@ -8,8 +8,79 @@ namespace knuth_morris_pratt
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Console.WriteLine("Input main string");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Input searched string");
+            string str2 = Console.ReadLine();
+
+
+            int position = FindSubstring(str1, str2);
+            Console.WriteLine(position);
+
+        }
+
+        private static int FindSubstring(string str1, string str2)
+        {
+            int n = str1.Length;
+            int m = str2.Length;
+
+            int[] d = GetPrefixFunction(str2);
+
+            int i = 0, j = 0;
+            while (j < m && i - j < n - m + 1)
+            {
+                if (str1[i] == str2[j])
+                {
+                    j++;
+                    i++;
+                }
+
+                else if (j == 0)
+                {
+                    i++;
+                }
+                else
+                {
+                    j = d[j - 1];
+                }
+
+            }
+
+            if (j == m)
+            {
+                Console.WriteLine("UnderIndex : ");
+                return i - j + 1;
+            }
+            else
+                return -1;
+        }
+
+        private static int[] GetPrefixFunction(string str2)
+        {
+            int length = str2.Length;
+            int[] result = new int[length];
+
+            int i = 1;
+            int j = 0;
+            result[0] = 0;
+            while (i <= length - 1)
+            {
+
+                if (str2[i] == str2[j])
+                {
+                    result[i] = j + 1;
+                    i++; j++;
+                }
+                else if (j == 0)
+                {
+                    result[i] = 0; i++;
+                }
+                else j = result[j - 1];
+            }
+
+            return result;
         }
     }
 }
